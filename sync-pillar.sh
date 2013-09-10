91eeb0ca9af3b10c90aa7a33ec6d8b15213d8dad
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-09-10 17:12:07 +0200

    Pillar sample

--- sync-pillar.sh
+++ sync-pillar.sh
@@ -1,7 +1,7 @@
 P="PILLAR.sample"
 cp real-pillar $P
 keys=""
-keys="$keys vhost mysql_root password token region tenant_name"
+keys="$keys vhost mysql_root password token region tenant_name availability_zone"
 keys="$keys project_password project_tenant project_user"
 for i in $keys;do
     sed -re "s/(.*$i)(=|:\s+).*/\1\2'xxxx'/g" -i $P
